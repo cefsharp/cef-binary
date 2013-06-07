910fc6018b045fa08be5763de4c29aa4a8d0e326
Per Lundberg <per.lundberg@ecraft.com>
2013-06-07 20:05:49 +0300

    Added binaries from cef_binary_1.1364.1123_windows.zip.
    Former-commit-id: f8aac12b7d1709a139ba8592420a051589bbfdc8

---
+++ cefclient/scheme_test.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2009 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#ifndef CEF_TESTS_CEFCLIENT_SCHEME_TEST_H_
+#define CEF_TESTS_CEFCLIENT_SCHEME_TEST_H_
+#pragma once
+
+#include "include/cef_browser.h"
+#include "include/cef_scheme.h"
+
+void AddSchemeTestSchemes(CefRefPtr<CefSchemeRegistrar> registrar);
+
+// Register the scheme handler.
+void InitSchemeTest();
+
+// Run the test.
+void RunSchemeTest(CefRefPtr<CefBrowser> browser);
+
+#endif  // CEF_TESTS_CEFCLIENT_SCHEME_TEST_H_
