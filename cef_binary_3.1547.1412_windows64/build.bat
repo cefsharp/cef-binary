67b051a9a0eb1bc7dcd193975b4cf0ed7f30f077
Per Lundberg <per.lundberg@ecraft.com>
2013-10-25 19:27:32 +0300

    Added build script, to simplify life a bit now that we have both VS2010/2012 (and soon 2013) to cater for.

---
+++ cef_binary_3.1547.1412_windows64/build.bat
@@ -0,0 +1,2 @@
+msbuild cefclient2010.sln /t:libcef_dll_wrapper:Rebuild /p:Configuration=Release /verbosity:Quiet
+msbuild cefclient2010.sln /t:libcef_dll_wrapper:Rebuild /p:Configuration=Debug /verbosity:Quiet
