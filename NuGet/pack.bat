bfee439bb492979dd7af869075c2f4879e046164
jornh <jornhansen@gmail.com>
2014-03-28 12:36:51 +0100

    cef.sdk.nuspec & friends used for cef.sdk.3.1650.1562-pre2 NuGet
    This is what was used to build the cef.sdk.3.1650.1562-pre2 NuGet currently on staging.nuget.org
    It comes with extra DRYness which CefSharp.Core now relies on since the SHA a7590937 commit https://github.com/jornh/CefSharp/commit/a7590937ccda3c4506827a91d81bd8093b672ec4
        <CefSdkVer>cef.sdk.3.1650.1562-pre2</CefSdkVer> in cef.sdk.props does that
    
    Note: Contents in the CEF/ folder is currently still a manual cherrypick from building upstream CEF
    Resolution of this is tracked in https://github.com/cefsharp/cef-binary/issues/5

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,3 +1,3 @@
-set version=3.1650.1562-pre0
+set version=3.1650.1562-pre2
 NuGet pack cef.redist.nuspec -NoPackageAnalysis -Version %version%
 NuGet pack cef.sdk.nuspec    -NoPackageAnalysis -Version %version%
