5278f736c7eb7f9cc24351750e9bf7a3ae4e908e
jornh <jornhansen@gmail.com>
2014-04-04 09:09:32 +0200

    cef.redist 3.1650.1562-pre3 NuGet ... currently only with x86 .dll files
    Former-commit-id: 2966877b847fd7e40912a80a4fd2d3c13bde2109

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,3 +1,5 @@
-set version=3.1650.1562-pre2
+set version=3.1650.1562-pre3
 NuGet pack cef.redist.nuspec -NoPackageAnalysis -Version %version%
+NuGet pack cef.redist.x86.nuspec -NoPackageAnalysis -Version %version%
+NuGet pack cef.redist.x64.nuspec -NoPackageAnalysis -Version %version%
 NuGet pack cef.sdk.nuspec    -NoPackageAnalysis -Version %version%
