5b7911a829af2d413fb0295674b59d98a17ee737
Peter Rekdal Sunde <peter.sunde@gmail.com>
2014-09-18 17:20:16 +0200

    make sure we don't add a BOM marker (visual studio will refuse to load the prop file)

--- build.ps1
+++ build.ps1
@@ -311,11 +311,14 @@ function Nupkg
         Die "Please install nuget. More information available at: http://docs.nuget.org/docs/start-here/installing-nuget"
     }
 
-    (Get-Content .\nuget\cef.sdk.props) `
-        -replace '<CefSdkVer>.*<\/CefSdkVer>', "<CefSdkVer>cef.sdk.$Version</CefSdkVer>" |
-        Out-File .\nuget\cef.sdk.props
     Write-Diagnostic "Building nuget package"
 
+    # Save content as UTF8 without adding BOM
+    $Filename = Resolve-Path ".\nuget\cef.sdk.props"
+    $Text = (Get-Content $Filename) -replace '<CefSdkVer>.*<\/CefSdkVer>', "<CefSdkVer>cef.sdk.$Version</CefSdkVer>"
+    [System.IO.File]::WriteAllLines($Filename, $Text)
+
+    # Build packages
     . $nuget pack nuget\cef.redist.nuspec -NoPackageAnalysis -Version $Version -OutputDirectory nuget
     . $nuget pack nuget\cef.sdk.nuspec -NoPackageAnalysis -Version $Version -OutputDirectory nuget
 }
