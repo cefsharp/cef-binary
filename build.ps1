f467e5d71706185cb470da101496d2fbf6c91e1c
amaitland <alex@maitlandcomputing.com.au>
2017-01-12 09:43:09 +1000

    Upgrade to 3.2883.1542.gf264ab9

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ $Cef32vcx = Join-Path (Join-Path $Cef32 'libcef_dll_wrapper') 'libcef_dll_wrappe
 $Cef64 = Join-Path $WorkingDir  'cef_binary_3.y.z_windows64'
 $Cef64vcx = Join-Path (Join-Path $Cef64 'libcef_dll_wrapper') 'libcef_dll_wrapper.vcxproj'
 
-$CefVersion = "3.2785.1486.g8c4ba9f"
+$CefVersion = "3.2883.1542.gf264ab9"
 # Take the cef version and strip the commit hash
 $CefPackageVersion = $CefVersion.SubString(0, $CefVersion.LastIndexOf('.'))
 
